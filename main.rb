a54a70ffeddc460062d12a46918bad498293704f
Gabriela gaba Rodriguez <gabelula@gmail.com>
2012-03-15 10:39:23 -0700

    Adds more paths to the app.

--- main.rb
+++ main.rb
@@ -1,17 +1,53 @@
+########## configuration & settings ###########
+configure do
+  set :name, ENV['NAME'] || 'DATA'
+  set :author, ENV['AUTHOR'] || 'Gaba'
+  set :analytics, ENV['ANALYTICS'] || 'uA-XXXXXXXX-X'
+  set :javascripts, %w[ ]
+  set :fonts, %w[ Abel ]
+  set :markdown, :layout_engine => :slim
+end
+
 ########## DAZ4126 website ###########
 require 'bundler'
 Bundler.require
 
 get '/' do
-  @title = "DAZ, Made in Manchester"
+  @title = "Nuestra Visión"
   slim :index
+end
+
+get '/mision' do
+  slim :mision
+end
+
+get '/quienes' do
+  slim :quienes
+end
+
+get '/que' do
+  slim :que
+end
+
+get '/contacto' do
+  slim :contacto
 end
 
 __END__
 ########### Views ###########
 
 @@index
-h1 title='Traditional Mancunian Greeting' Alright Mate!
-p  Welcome to my website! My name is DAZ and I work, rest and play in Manchester,UK.
-p I build websites, play water polo and eat burgers.
+h1 title='Nuestra Visión'
 p Thanks for visiting. Have a nice day!
+
+@@mision
+p Mision
+
+@@quienes
+p Quienes somos
+
+@@que
+p Que Hacemos
+
+@@contacto
+p Contacto
